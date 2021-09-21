596676a204cf78ae2409761c796ba1462905e0ef
arpanosso <arpanosso@yahoo.com.br>
2021-09-21 12:41:48 -0300

    read.table rulex

--- R/atualizar_dados_nooa.R
+++ R/atualizar_dados_nooa.R
@@ -16,20 +16,10 @@
 #'   ggplot2::theme_minimal()
 atualizar_dados_nooa <- function(){
   url <- "https://gml.noaa.gov/webdata/ccgg/trends/co2/co2_weekly_mlo.txt"
-  df <- readr::read_table(url, col_names = TRUE) %>%
-    janitor::clean_names() %>%
-    dplyr::filter(!stringr::str_detect(number, "#")) %>%
-    dplyr::select(x) %>%
-    dplyr::mutate(
-      x = stringr::str_squish(x)
-    ) %>%
-    dplyr::pull(x) %>%
-    stringr::str_split(" ",simplify = TRUE) |>
-    tibble::as_tibble() %>%
-    dplyr::mutate_if(is.character,as.numeric)
-
- names(df) <- c("year", "month", "day",
+  df <- read.table(url)
+  names(df) <- c("year", "month", "day",
                 "decimal", "CO2_ppm", "n_days",
                 "year_ago_1", "years_ago_10", "since_1800")
  df
 }
+
